5570c29b57a676322367c529074799c6274671a1
admin <you@example.com>
2024-08-08 07:55:08 +0200

    initial commit

---
+++ LeetCodeSolutions/Arrays/TwoSum.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeSolutions.Arrays;
+
+public class TwoSum
+{
+    public int[] Solve(int[] nums, int target)
+    {
+        var map = new Dictionary<int, int>();
+        int[] result = new int[] { };
+
+        for (var i = 0; i <= nums.Length; i++)
+        {
+            var currentValue = nums[i];
+            var complement = target - currentValue;
+
+            if (map.ContainsKey(complement))
+            {
+                result = new int[] { map[complement], i };
+                break;
+            }
+
+            map[nums[i]] = i;
+        }
+
+        return result;
+    }
+}
