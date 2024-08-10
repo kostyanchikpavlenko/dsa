c273e9819cb1a990b07fe0968d5e15496350dec1
admin <you@example.com>
2024-08-10 14:02:41 +0200

    Metadata has been updated

--- LeetCodeSolutions/Arrays/TwoSum.cs
+++ LeetCodeSolutions/Arrays/TwoSum.cs
@@ -1,6 +1,14 @@
 ﻿using System.Collections.Generic;
 
 namespace LeetCodeSolutions.Arrays;
+/// <summary>
+/// Runtime complexity: 0(n)
+/// Space complexity: 0(n)
+/// Runtime: 116ms
+/// Memory: 48.23 MB
+/// Solution pattern: Dictionary
+/// Level: Easy
+/// </summary>
 
 public class TwoSum
 {
