518eed8c7185cd437ac197ed7ea2a839ac76238e
admin <you@example.com>
2024-08-20 19:07:17 +0200

    BestTimeToBuyAndSellStock solution added

---
+++ LeetCodeSolutions/Arrays/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCodeSolutions.Arrays;
+
+public class BestTimeToBuyAndSellStock
+{
+    public int MaxProfit(int[] prices)
+    {
+        if (prices.Length < 2)
+        {
+            return 0;
+        }
+
+        int maxProfit = 0;
+        int minPrice = prices[0];
+
+        for (int i = 0; i < prices.Length; i++)
+        {
+            var currentProfit = prices[i] - minPrice;
+
+            if (currentProfit > maxProfit)
+            {
+                maxProfit = currentProfit;
+            }
+
+            if (prices[i] < minPrice)
+            {
+                minPrice = prices[i];
+            }
+        }
+
+        return maxProfit;
+    }
+}
