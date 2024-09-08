9ccad683b5a4abb507969f6331df18a9f3394bf8
admin <you@example.com>
2024-09-08 15:49:47 +0200

    NumSubArraysProductLessThanK added

---
+++ LeetCodeSolutions/Arrays/NumSubArraysProductLessThanK.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeSolutions.Arrays;
+
+public class NumSubArraysProductLessThanK
+{
+    public int FindNumSubarrayProductLessThanK(int[] nums, int k) 
+    {
+        if (k == 0)
+        {
+            return 0;
+        }
+        
+        int subArraysCount = 0;
+        int startWindow = 0;
+        int product = 1;
+
+        for(int endWindow = 0; endWindow < nums.Length; endWindow++)
+        {
+            product *= nums[endWindow];
+
+            while(product >= k)
+            {
+                product /= nums[startWindow];
+                startWindow++;
+            }
+
+            subArraysCount += (endWindow - startWindow) + 1;
+        }
+        
+        return subArraysCount;
+    }
+}
