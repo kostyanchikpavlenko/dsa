de3e59e6de8cc6e7d43745eb4339bd7ac1bfcf31
admin <you@example.com>
2024-12-12 16:52:00 +0100

    Structure of solution has been updated

--- DataStructureConsoleTests/Program.cs
+++ DataStructureConsoleTests/Program.cs
@@ -1,32 +1,168 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 
-using DataStructures.LinkedLists;
+// Example: Create a linked list with values [1, 2, 3, 4, 5] and a cycle starting at index 2 (value 3)
 
-var list = new SinglyLinkedList<int>();
+LinkedListWithCycle linkedList = new LinkedListWithCycle(new int[] { 1,2}, 0);
 
-var node1 = Node<int>.Create(1);
-var node2 = Node<int>.Create(2);
-var node3 = Node<int>.Create(3);
+// The list will have a cycle: 1 -> 2 -> 3 -> 4 -> 5 -> (points back to 3)
+ListNode head = linkedList.Head;
 
-list.AddLast(node1);
-list.AddLast(node2);
-list.AddLast(node3);
+var length = FindCycleLength(head);
+var result = HasCycle(head);
+var node = FindCycleStart(head);
 
+Console.WriteLine();
 
-list.Remove(node1);
-
-
-
-
-
-//
-// var result = list.Find(5);
-// var result2 = list.Find(6);
-//
-// var first = list.GetFirst();
-// var last = list.GetLast();
-
-foreach (var item in list)
+ListNode FindCycleStart(ListNode head)
 {
-    Console.WriteLine(item.Value);
+    ListNode slow = head;
+    ListNode fast = head;
+
+    while (fast is not null && fast.Next is not null)
+    {
+        slow = slow.Next;
+        fast = fast.Next.Next;
+
+        if (slow == fast)
+        {
+            break;
+        }
+    }
+
+    slow = head;
+    while (slow != fast)
+    {
+        if (slow == fast)
+        {
+            return slow;
+        }
+
+        slow = slow.Next;
+        fast = fast.Next;
+    }
+
+    return null;
+}
+
+// Optional: Implement a cycle detection algorithm here to verify the cycle
+bool HasCycle(ListNode head)
+{
+    if (head is null || head.Next is null || head.Next.Next is null)
+    {
+        return false;
+    }
+
+    ListNode slowPointer = head;
+    ListNode fastPointer = head;
+
+    while (fastPointer is not null &&
+            fastPointer.Next is not null)
+    {
+        slowPointer = slowPointer.Next;
+        fastPointer = fastPointer.Next.Next;
+        
+        if (slowPointer == fastPointer)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int FindCycleLength(ListNode head)
+{
+    if (head?.Next is null
+        || head.Next.Next is null)
+    {
+        return 0;
+    }
+    
+
+    ListNode slowPointer = head;
+    ListNode fastPointer = head;
+
+    while (fastPointer is not null && fastPointer.Next is not null)
+    {
+        slowPointer = slowPointer.Next;
+        fastPointer = fastPointer.Next.Next;
+
+        if (slowPointer == fastPointer)
+        {
+            return CalculateCycleLength(slowPointer);
+        }
+    }
+
+    return 0;
+}
+
+int CalculateCycleLength(ListNode slow)
+{
+    ListNode current = slow;
+
+    int cycleLength = 1;
+
+    slow = slow.Next;
+    while (current != slow)
+    {
+        slow = slow.Next;
+        cycleLength++;
+    }
+
+    return cycleLength;
+}
+
+public class ListNode
+{
+    public int Value { get; set; }
+    public ListNode Next { get; set; }
+
+    public ListNode(int value)
+    {
+        Value = value;
+        Next = null;
+    }
+}
+
+
+
+public class LinkedListWithCycle
+{
+    public ListNode Head { get; private set; }
+
+    public LinkedListWithCycle(int[] values, int cycleIndex)
+    {
+        if (values.Length == 0) return;
+
+        ListNode current = null;
+        ListNode cycleEntryNode = null;
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            ListNode newNode = new ListNode(values[i]);
+
+            if (Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                current.Next = newNode;
+            }
+
+            current = newNode;
+
+            // Save the cycle entry node
+            if (i == cycleIndex)
+            {
+                cycleEntryNode = current;
+            }
+        }
+
+        // Create the cycle
+        if (cycleEntryNode != null)
+        {
+            current.Next = cycleEntryNode;
+        }
+    }
 }
